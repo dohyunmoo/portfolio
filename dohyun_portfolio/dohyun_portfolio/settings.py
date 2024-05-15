13ed19a16b0e5fb4439eafce0a8e526e316416e8
Dohyun Moon <d6moon@uwaterloo.ca>
2024-05-15 08:46:50 -0400

    models added and settings configured

--- dohyun_portfolio/dohyun_portfolio/settings.py
+++ dohyun_portfolio/dohyun_portfolio/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "main",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
