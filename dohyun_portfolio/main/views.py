55ea4e36e70e4249c7fafb1ebe0ccdd008dc9e04
Dohyun Moon <d6moon@uwaterloo.ca>
2024-05-19 16:13:22 -0400

    update

--- dohyun_portfolio/main/views.py
+++ dohyun_portfolio/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Project, Tag
 
-# Create your views here.
+
+def home(request):
+    return render(request, "home.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def project(request, id):
+    return render(request, "project.html")
